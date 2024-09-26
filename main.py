e05f8bf9d2ecc5e4c95295da886e73319c9ec05a
gcancel <gmartin9892@gmail.com>
2024-09-26 01:17:32 -0500

    added player and CircleShape classes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from constants import *
+from player import Player
 
 def main():
     print('Starting asteroids!')
@@ -9,13 +10,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    fps = pygame.time.Clock()
+    dt = 0
+    
+    player_pos_x = SCREEN_WIDTH / 2
+    player_pos_y = SCREEN_HEIGHT /2
+    ship = Player(player_pos_x, player_pos_y)
+    
+    
     while True:
+        # enable quit for game 
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 pygame.quit();sys.exit()
                 return
-                
+            
+        # draw ship on screen        
         screen.fill('black')
+        ship.draw(screen)
+        pygame.display.flip()
+        
+        # limit framerate to 60 fps
+        dt = fps.tick(FPS) / 1000
 
 if __name__ =='__main__':
         main()
